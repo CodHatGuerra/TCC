94819837ed4e95d0ac57a4cae4dff1378e9cfd55
Gabriel Guerra <inlive04@outlook.com>
2023-04-04 10:20:44 -0300

    Adicionando Rota GET API REST

--- Aplicação/BackEnd/index.js
+++ Aplicação/BackEnd/index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json()); // configura o middleware para o formato JSON
 app.use(cors()); // configura o middleware do CORS para permitir requisições de qualquer origem
 
-app.post('/teste', (req, res) => {
+app.post("/post", (req, res) => {
   const body = req.body;
   console.log(body); // exibe o corpo da requisição no console
-  res.json({ message: 'Dados recebidos com sucesso!' }); // envia uma resposta JSON para o cliente
+  res.json({ message: "Dados recebidos com sucesso!" }); // envia uma resposta JSON para o cliente
+});
+
+app.get("/get", (req, res) => {
+  const informacoes = {
+    nome: "João Silva",
+    cpf: "123.456.789-10",
+    email: "joao.silva@example.com",
+    telefone: "(11) 98765-4321",
+  };
+  res.json(informacoes);
 });
 
 app.listen(8080, () => {
-  console.log('Servidor iniciado na porta 8080');
+  console.log("Servidor iniciado na porta 8080");
 });
