e0db1fec5e4f47a836a1581a374aab8c78836e5a
CodHatGuerra <105330787+CodHatGuerra@users.noreply.github.com>
2023-04-22 14:58:44 -0300

    Subindo Banco de dados Completo

---
+++ Aplicação/Banco de Dados/SCRIPTS/Inserção.sql
@@ -0,0 +1,7 @@
+INSERT INTO TB_Usuario (Nome, Sobrenome, Email, Senha, DataNascimento, Permissao)
+VALUES
+('Lucas', 'Silva', 'lucas.silva@example.com', 'senha123', '1990-05-20', 1),
+('Ana', 'Santos', 'ana.santos@example.com', 'senha456', '1995-02-15', 0),
+('João', 'Souza', 'joao.souza@example.com', 'senha789', '1993-11-30', 1),
+('Carla', 'Ferreira', 'carla.ferreira@example.com', 'senhaabc', '1998-06-10', 0),
+('Pedro', 'Lima', 'pedro.lima@example.com', 'senhaxyz', '1991-09-05', 0);
