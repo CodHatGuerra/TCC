071671e68059e62eea269453ed56b9eaec13bb8a
Gabriel Guerra <inlive04@outlook.com>
2023-05-08 16:47:54 -0300

    Ajuste API

--- Aplicação/BackEnd/src/server.js
+++ Aplicação/BackEnd/src/server.js
@@ -1,18 +1,22 @@
 require("dotenv").config({ path: "variaveis.env" });
 const express = require("express");
+const session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const routes = require("./routes");
 
 const server = express();
+
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use(session({ secret: "segredo" }));
 server.use(bodyParser.json());
 server.use("/api", routes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Servidor rodando em: http://localhost:${process.env.PORT}/`);
-  console.log(`Rota para cadastrar pessoa: http://localhost:8080/api/cadastrar`);
+  console.log(
+    `Rota para cadastrar pessoa: http://localhost:8080/api/cadastrar`
+  );
   console.log(`Rota para Verificar Login: http://localhost:8080/api/login`);
 });
