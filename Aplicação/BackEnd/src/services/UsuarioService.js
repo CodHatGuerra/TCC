3d4982c78cb7f806cfc3b0715870c1773fa87a3b
Gabriel Guerra <inlive04@outlook.com>
2023-05-18 17:25:39 -0300

    AJustes API e Banco

--- Aplicação/BackEnd/src/services/UsuarioService.js
+++ Aplicação/BackEnd/src/services/UsuarioService.js
@@ -4,7 +4,6 @@ module.exports = {
   inserir: (usuario, endereco, celular) => {
     return new Promise((aceito, rejeitado) => {
       let ID_Endereco = null;
-      let ID_Celular = null;
 
       db.query(
         "INSERT INTO tb_usuario (Nome, Cpf, Rg, Sexo, Data_Nascimento, Email, Senha, data_Criada) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
@@ -25,7 +24,7 @@ module.exports = {
             return;
           }
           db.query(
-            "(INSERT INTO tb_endereco (Cep, Uf, Localidade, Bairro, Logradouro, Numero) VALUES (?, ?, ?, ?, ?, ?)",
+            "INSERT INTO tb_endereco (Cep, Uf, Localidade, Bairro, Logradouro, Numero) VALUES (?, ?, ?, ?, ?, ?)",
             [
               endereco.cep,
               endereco.uf,
@@ -36,33 +35,47 @@ module.exports = {
             ],
             (error, result) => {
               if (error) {
+                console.log("ERRO ENDEREÇO");
                 console.log(error);
                 rejeitado(error);
                 return;
               }
-
               if (result) {
-                console.log(result);
                 ID_Endereco = result.insertId;
               }
             }
           );
           db.query(
-            "(INSERT INTO tb_celular (Numero) VALUES (?)",
-            [celular.numero],
+            "INSERT INTO tb_celular (Numero, TB_Usuario_ID_Usuario) VALUES (?, ?)",
+            [celular.numero, results.insertId],
             (error, result) => {
               if (error) {
+                console.log("ERRO NUMERO");
                 console.log(error);
                 rejeitado(error);
                 return;
               }
               if (result) {
-                console.log(result);
                 ID_Endereco = result.insertId;
               }
             }
           );
 
+          db.query(
+            "INSERT INTO tb_usuario (ID_UsuarioNumero, , TB_Endereco_ID_Endereco  VALUES (?, ?)",
+            [results.insertId, ID_Endereco],
+            (error, result) => {
+              if (error) {
+                console.log("ERRO NUMERO");
+                console.log(error);
+                rejeitado(error);
+                return;
+              }
+              if (result) {
+                ID_Endereco = result.insertId;
+              }
+            }
+          );
           aceito(results.insertId);
         }
       );
