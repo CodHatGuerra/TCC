5125b4e89adb0be1a8dc91d08253eae85e0e7df9
unknown <elielelano7@gmail.com>
2023-08-09 22:26:39 -0300

    commit

--- Aplicação/FrontEnd/src/app/components/register/register.component.ts
+++ Aplicação/FrontEnd/src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environments';
 
 
@@ -19,41 +19,18 @@ export class RegisterComponent implements OnInit {
       this.GetPostos();
     }
 
+    
+      dados = [] 
+
     GetPostos(){
       this.http.get(`${environment.baseUrl}/${environment.Posto}`).subscribe(
         (response)=>{
-          console.log(response);
+          this.dados = response as any[];
         }
       )
     }
 
-  pessoas = [
-    {
-     nome: 'Posto1',
-     numero: '26',
-     profissao: 'Advogada',
-     cidade: 'bariri'
-    },
-    {
-     nome: 'Posto 2',
-     numero: '27',
-     profissao: 'Desenvolvedor de software',
-     cidade: 'bauru'
-    },
-    {
-     nome: 'Posto 3',
-     numero: '24',
-     profissao: 'Veterinária',
-     cidade: 'barra'
-    },
-    {
-     nome: 'Posto 4',
-     numero: '30',
-     profissao: 'Médica',
-     cidade: 'gotham'
-    },
-]
-colunas: string[] = ['nome', 'numero', 'profissao', 'cidade'];
+colunas: string[] = ['nome', 'bairro', 'localidade'];
 };
 
  
