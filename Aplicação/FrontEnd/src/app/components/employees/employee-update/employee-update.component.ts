0e6d746d4903ece9fc4761bde33e4269829f83f4
unknown <elielelano7@gmail.com>
2023-11-07 20:41:07 -0300

    commit

--- Aplicação/FrontEnd/src/app/components/employees/employee-update/employee-update.component.ts
+++ Aplicação/FrontEnd/src/app/components/employees/employee-update/employee-update.component.ts
@@ -2,6 +2,7 @@ import { MatDialogRef } from "@angular/material/dialog";
 import { Component, OnInit } from "@angular/core";
 import { EmployeeService } from "../../../settings/Services/employees-service.service";
 import { Router } from "@angular/router";
+import { PostosService } from "src/app/settings/Services/postos.service";
 
 @Component({
   selector: "app-employee-update",
@@ -12,19 +13,38 @@ export class EmployeeUpdateComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<EmployeeUpdateComponent>,
     private employeeService: EmployeeService,
+    private postoService: PostosService,
     private router: Router
-  ) {}
+  ) {
+    this.getPosto();
+  }
 
     employee: any;
+    idPosto: number = 0;
+    postos: any;
 
   ngOnInit(): void {
     const id = this.employeeService.getIdFuncionario();
     console.log(id);
-    this.employeeService.getByIdEmployee(id).subscribe((result)=>{
-      console.log(result);
-       this.employee = result.result.funcionario
+    this.employeeService.getByIdEmployee(id).subscribe((response)=>{
+      this.employee = response.result.funcionario[0]
+      console.log(this.employee);
+    });
+    this.getPosto();
+  }
+
+  onSelectionChange(event: any) {
+    this.idPosto = event.value;
+    this.employeeService.setIdPosto(event.value);
+    //this.getEmployeesByPosto(this.idPosto);
+  }
+
+  getPosto() {
+    this.postoService.GetPosto().subscribe((response) => {
+      this.postos = response.result.postos;
     });
   }
+
 
   // UpdateEmployee() {
   //   this.employeeService.updateEmployee();
